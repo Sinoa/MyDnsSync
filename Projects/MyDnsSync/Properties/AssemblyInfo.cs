d37f431e13412df2cb803880f3d78f10b05c5a7a
Sinoa <sinoa.dev@gmail.com>
2018-10-08 20:14:40 +0900

    プロジェクト新規作成

---
+++ Projects/MyDnsSync/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("MyDnsSync")]
+[assembly: AssemblyDescription("MyDNSへIPアドレスの通知を行うアプリケーションです")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sinoa")]
+[assembly: AssemblyProduct("MyDnsSync")]
+[assembly: AssemblyCopyright("Copyright © Sinoa 2018")]
+[assembly: AssemblyTrademark("Sinoa")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、 
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("13a36345-a766-49bf-ae93-a93a7188dbfe")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Projects/MyDnsSync/src/ApplicationMain.cs
@@ -0,0 +1,64 @@
+﻿// zlib/libpng License
+//
+// Copyright (c) 2018 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+
+namespace MyDnsSync
+{
+    /// <summary>
+    /// アプリケーションメインクラスです。
+    /// アプリケーションの起動、初期化、終了を行います。
+    /// </summary>
+    internal sealed class ApplicationMain : IDisposable
+    {
+        /// <summary>
+        /// アプリケーションのエントリポイントです
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            // アプリケーションメインのインスタンスを生成
+            using (var main = new ApplicationMain())
+            {
+                // アプリケーションを開始する
+                main.Run();
+            }
+        }
+
+
+        /// <summary>
+        /// ApplicationMain のインスタンスを初期化します
+        /// </summary>
+        private ApplicationMain()
+        {
+        }
+
+
+        /// <summary>
+        /// ApplicationMain のリソースを解放します
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+
+        /// <summary>
+        /// アプリケーションの動作を開始します
+        /// </summary>
+        private void Run()
+        {
+        }
+    }
+}
