d11c92c9c9dfe78aa3f69c88c165212214a7d826
Sinoa <sinoa.dev@gmail.com>
2018-10-21 04:06:19 +0900

    スケジューラFIX

--- Projects/MyDnsSync/Components/SyncScheduler.cs
+++ Projects/MyDnsSync/Components/SyncScheduler.cs
@@ -24,8 +24,53 @@ namespace MyDnsSync.Components
     /// </summary>
     public partial class SyncScheduler : Component
     {
+        /// <summary>
+        /// スケジュール内容を保持した構造体です
+        /// </summary>
+        private struct ScheduleContent
+        {
+            // メンバ変数定義
+            private Action<object> callback;
+            private object state;
+
+
+
+            /// <summary>
+            /// スケジュールされた日時
+            /// </summary>
+            public DateTime ScheduleDateTime { get; private set; }
+
+
+
+            /// <summary>
+            /// ScheduleContent インスタンスを初期化します
+            /// </summary>
+            /// <param name="scheduleDateTime">callback を呼ぶ日時</param>
+            /// <param name="callback">スケジュールされた日時が来た時に呼び出すコールバック関数</param>
+            /// <param name="state">コールバック関数に渡す状態変数</param>
+            public ScheduleContent(DateTime scheduleDateTime, Action<object> callback, object state)
+            {
+                // 受け取る
+                this.callback = callback;
+                this.state = state;
+                ScheduleDateTime = scheduleDateTime;
+            }
+
+
+            /// <summary>
+            /// コーバックを呼びます
+            /// </summary>
+            public void Invoke()
+            {
+                // コールバックを呼ぶ
+                callback(state);
+            }
+        }
+
+
+
         // メンバ変数定義
-        private List<DateTime> scheduleList;
+        private List<ScheduleContent> scheduleList;
 
 
 
@@ -50,7 +95,7 @@ namespace MyDnsSync.Components
 
 
             // スケジュールリストを生成
-            scheduleList = new List<DateTime>();
+            scheduleList = new List<ScheduleContent>();
         }
 
 
@@ -61,6 +106,23 @@ namespace MyDnsSync.Components
         /// <param name="e">イベント内容</param>
         private void OnWatchTimerTick(object sender, EventArgs e)
         {
+            // スケジュールリストの末尾から回る
+            for (int i = scheduleList.Count - 1; i >= 0; --i)
+            {
+                // もし実行すべき日時を迎えていないなら
+                if (scheduleList[i].ScheduleDateTime > DateTime.Now)
+                {
+                    // ループから抜ける
+                    break;
+                }
+
+
+                // イベントを起こして削除する
+                scheduleList[i].Invoke();
+                scheduleList.RemoveAt(i);
+            }
+
+
             // もしスケジュールリストが空なら
             if (scheduleList.Count == 0)
             {
@@ -72,14 +134,16 @@ namespace MyDnsSync.Components
 
 
         /// <summary>
-        /// 指定された日付をスケジュールします
+        /// 指定されたコールバックを実行する為の、日付をスケジュールします
         /// </summary>
         /// <param name="eventDateTime">スケジュールする日付</param>
-        public void Schedule(DateTime eventDateTime)
+        /// <param name="callback">実行するコールバック</param>
+        /// <param name="state">コールバックに渡す状態変数</param>
+        public void Schedule(DateTime eventDateTime, Action<object> callback, object state)
         {
-            // リストに追加してソートする
-            scheduleList.Add(eventDateTime);
-            scheduleList.Sort();
+            // リストに追加して日付の降順でソートする
+            scheduleList.Add(new ScheduleContent(eventDateTime, callback, state));
+            scheduleList.Sort((a, b) => DateTime.Compare(a.ScheduleDateTime, b.ScheduleDateTime) * -1);
 
 
             // 監視タイマーを起こす
